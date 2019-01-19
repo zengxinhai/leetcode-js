0d7675d2a09f486dace3f99ac5a1e7627a0ac2dd
IceFox <zengxinhai001@163.com>
2019-01-19 14:14:44 +0800

    Optmize solution: binary-tree-inorder-traversal

--- solutions/binary-tree-inorder-traversal/solution.js
+++ solutions/binary-tree-inorder-traversal/solution.js
@@ -10,6 +10,19 @@
  * @return {number[]}
  */
 
+
+/**
+* Definition for a binary tree node.
+* function TreeNode(val) {
+*     this.val = val;
+*     this.left = this.right = null;
+* }
+*/
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
 const inorderTraversal = (root) => {
   const steps = [[root, 0]];
   const ans = [];
@@ -21,9 +34,22 @@ const inorderTraversal = (root) => {
     if (type === 1) {
       ans.push(node.val);
     } else if (type === 0) {
+      // In-order
       steps.push([node.right, 0]);
       steps.push([node, 1]);
       steps.push([node.left, 0]);
+
+      /** Pre-order
+      steps.push([node.right, 0]);
+      steps.push([node.left, 0]);
+      steps.push([node, 1]);
+      */
+
+      /** Post-order
+      steps.push([node, 1]);
+      steps.push([node.right, 0]);
+      steps.push([node.left, 0]);
+      */
     }
   }
   return ans;
